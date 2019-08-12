7ca00a984a52c81fa29115ba6a922dc474beb671
raviSussol <ravi.shrestha@sussol.net>
2019-08-12 18:21:10 +0545

    Create functions

--- src/contracts/MedicalRecord.sol
+++ src/contracts/MedicalRecord.sol
@@ -45,7 +45,51 @@ contract MedicalRecord {
   }
   mapping(address => Medication) public medications;
 
-  function createMedication() public {
+  function createAllergy(
+    string memory _title,
+    string memory _medicationType,
+    string memory _medicalCode,
+    string memory _beginDate,
+    string memory _endDate,
+    string memory _occurance,
+    string memory _referredBy,
+    string memory _outcome,
+    string memory _comments) public {
+    medications[msg.sender] = Medication(
+      _id,
+      _title,
+      _medicationType,
+      _medicalCode,
+      _beginDate,
+      _endDate,
+      _occurance,
+      _referredBy,
+      _outcome,
+      _comments
+    );
+  }
 
+  function createMedication(
+    string memory _title,
+    string memory _medicationType,
+    string memory _medicalCode,
+    string memory _beginDate,
+    string memory _endDate,
+    string memory _occurance,
+    string memory _referredBy,
+    string memory _outcome,
+    string memory _comments) public {
+    medications[msg.sender] = Medication(
+      _id,
+      _title,
+      _medicationType,
+      _medicalCode,
+      _beginDate,
+      _endDate,
+      _occurance,
+      _referredBy,
+      _outcome,
+      _comments
+    );
   }
 }
